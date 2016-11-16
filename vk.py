ebcde77756bd4e1e2fd0bc5543a5173b890398ff
Alexander Petrov <Alexander Petrov>
2016-11-16 11:47:14 +0100

    added tqdm

--- vk.py
+++ vk.py
@@ -5,6 +5,8 @@ import json
 from requests.exceptions import ReadTimeout
 import networkx as nx
 from pymongo import MongoClient
+from tqdm import tqdm
+
 mongo = MongoClient()
 db = mongo.vk_cache
 table = db.vk_cache
@@ -44,25 +46,35 @@ def get_friends(user_id):
     else:
         return res
 
-def get_users_info(users_list):
+def get_users_info(users_list, fields=None, show_pbar=None):
     result = {}
     batch_size = 500
-    fields = ",".join(["sex", "bdate", "city", "country", "photo_50", "photo_100", "photo_200_orig",
-            "photo_200", "photo_400_orig", "photo_max", "photo_max_orig", "photo_id", "online",
-            "online_mobile", "domain", "has_mobile", "contacts", "connections", "site", "education",
-            "universities", "schools", "can_post", "can_see_all_posts", "can_see_audio",
-            "can_write_private_message", "status", "last_seen", "relation",
-            "relatives", "counters", "screen_name", "maiden_name", "timezone", "occupation,activities",
-            "interests", "music", "movies", "tv", "books", "games", "about", "quotes", "personal",
-            "friends_status"])
+    if fields is None:
+        fields = ",".join(["sex", "bdate", "city", "country", "photo_50", "photo_100", "photo_200_orig",
+                "photo_200", "photo_400_orig", "photo_max", "photo_max_orig", "photo_id", "online",
+                "online_mobile", "domain", "has_mobile", "contacts", "connections", "site", "education",
+                "universities", "schools", "can_post", "can_see_all_posts", "can_see_audio",
+                "can_write_private_message", "status", "last_seen", "relation",
+                "relatives", "counters", "screen_name", "maiden_name", "timezone", "occupation,activities",
+                "interests", "music", "movies", "tv", "books", "games", "about", "quotes", "personal",
+                "friends_status"])
     start = 0
+    if show_pbar:
+        pbar = tqdm(total=len(users_list))
+
     while start < len(users_list):
+        if show_pbar:
+            pbar.update(batch_size)
         request_ids = ",".join([str(id) for id in users_list[start:start+batch_size]])
         data = base_request("users.get", [("fields", fields), ("user_ids", request_ids)], True, timeout=5)
         for item in data:
-            result[str(item['uid'])] = item
+            yield item
         start += batch_size
-    return result
+
+
+def get_n_users(users_cnt, fields=None, show_pbar=False, start_index=1):
+    users_list = list(range(start_index, start_index+users_cnt))
+    return get_users_info(users_list, fields, show_pbar)
 
 def get_social_ball(user_id, ball_size):
     queue = [(user_id, 0)]
